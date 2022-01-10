74ee2ca44966be3ef41008a6c9440999958ff307
Dalton <slattery.da@gmail.com>
2022-01-10 15:53:55 -0600

    commit tree ef3da93cc425af04917e718bd542ba71dad72ffd

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Dalton Slattery';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
